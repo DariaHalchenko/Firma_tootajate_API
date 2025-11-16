e5be51e862f79ec4f76b468e01ab8113dee05f0f
DariaHalchenko <daragalcenko3@gmail.com>
2025-11-16 20:41:31 +0300

    muuta

--- Firma_tootajate_API/Models/Tootajate.cs
+++ Firma_tootajate_API/Models/Tootajate.cs
@@ -1,6 +1,14 @@
 ﻿namespace Firma_tootajate_API.Models
 {
-    public class Tootajatecs
+    public class Tootajate
     {
+        public int Id { get; set; }           
+        public string Nimi { get; set; }           
+        public string Isikukood { get; set; }      
+        public string Amet { get; set; }          
+        public decimal Tunnitasu { get; set; }  
+        public string Email { get; set; }          
+        public string Parool { get; set; }        
+        public bool IsAdmin { get; set; } = false; 
     }
 }
